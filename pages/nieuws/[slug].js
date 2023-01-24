5b83a3bc08053aa98f3ffb46117615fe7f1a61f6
dylanvanzanten <info@dylanvanzanten.nl>
2023-01-24 14:46:45 +0100

    refactor(Article): Add author data, add Next Image component

--- pages/nieuws/[slug].js
+++ pages/nieuws/[slug].js
@@ -1,5 +1,6 @@
 import { getEntries } from "./../api/contentful";
 import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+import Image from "next/image";
 
 export const getStaticPaths = async () => {
   const news = await getEntries({ content_type: "nieuws" });
@@ -28,42 +29,84 @@ export async function getStaticProps({ params }) {
 }
 
 const Article = ({ news }) => {
-  console.log(news);
-  const { title, slug, teaserImg, category, author, articleContent, link } =
-    news.fields;
+  const { title, articleImg, category, author, articleContent } = news.fields;
+  console.log(author);
   return (
     <>
-      <main>
-        <div>
-          <h1>{title}</h1>
-          {news.fields.author && (
-            <>
-              {author.map((author) => (
-                <p key={author} className="article-teaser__author">
-                  {author.fields.name}
-                </p>
-              ))}
-            </>
+      <div className="article">
+        <main>
+          {news.fields.articleImg && (
+            <picture className="article__img-wrapper">
+              <source
+                srcSet={`https:${articleImg.fields.file.url}?fit=scale&h=300&fm=webp`}
+                media="(min-width: 960px)"
+                type="image/webp"
+              />
+              <source
+                srcSet={`https:${articleImg.fields.file.url}?fit=fill&h=300&w=300&fm=webp`}
+                media="(min-width: 640px)"
+                type="image/webp"
+              />
+              <source
+                srcSet={`https:${articleImg.fields.file.url}?fit=fill&h=300&w=300&fm=webp`}
+                media="(max-width: 639px)"
+                type="image/webp"
+              />
+              <source
+                srcSet={`https:${articleImg.fields.file.url}?fit=scale&h=250&fm=webp`}
+                media="(max-width: 479px)"
+                type="image/webp"
+              />
+              <img
+                className="article-teaser__img"
+                src={`https:${articleImg.fields.file.url}`}
+                alt={articleImg.fields.title}
+              />
+            </picture>
           )}
-          {news.fields.category && (
-            <div className="article-teaser__category-container">
-              {category.map((category) => (
-                <span
-                  key={category}
-                  className={`article-teaser__label article-teaser__category`}
-                >
-                  {category}
-                </span>
-              ))}
-            </div>
-          )}
-          <div
-            dangerouslySetInnerHTML={{
-              __html: documentToHtmlString(articleContent),
-            }}
-          />
-        </div>
-      </main>
+          <article className="article__content">
+            <h1>{title}</h1>
+            {news.fields.author && (
+              <div className="article-teaser__author-container">
+                {author.map((author) => (
+                  <>
+                    <p key={author.sys.id} className="article-teaser__author">
+                      {author.fields.name}
+                    </p>
+                    <Image
+                      src={`https:${author.fields.avatar.fields.file.url}`}
+                      alt={author.fields.avatar.fields.title}
+                      quality={100}
+                      blurDataURL={`https:${author.fields.avatar.fields.file.url}`}
+                      placeholder="blur"
+                      width={20}
+                      height={20}
+                      sizes="100vw"
+                    />
+                  </>
+                ))}
+              </div>
+            )}
+            {news.fields.category && (
+              <div className="article-teaser__category-container">
+                {category.map((category) => (
+                  <span
+                    key={category}
+                    className={`article-teaser__label article-teaser__category`}
+                  >
+                    {category}
+                  </span>
+                ))}
+              </div>
+            )}
+            <div
+              dangerouslySetInnerHTML={{
+                __html: documentToHtmlString(articleContent),
+              }}
+            />
+          </article>
+        </main>
+      </div>
     </>
   );
 };
